4b6a8aaabb80da18125b04ad5d3b0b022dce2337
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-08-15 17:59:51 +0530

    Base_Ready

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
-#This is my demo.py file
+import time
+start = time.perf_counter()
 
-def add(n, m):
-    return n+m
-res = add(10, 20)
-print(res)
+def do_something():
+    print('Sleeping for 1 sec...')
+    time.sleep(1)
+    print('Done Sleeping.')
 
-def sub(n, m):
-    return n-m
-res = sub(20, 10)
-print(res)
+do_something()
+do_something()
+
+finish = time.perf_counter()
+print(f'Finished in {finish-start} seconds.')
